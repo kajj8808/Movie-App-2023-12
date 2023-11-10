9d2fbd8d8359dba3933bfa603d0dda5f5230472e
강범창 <kang@gangbeomchang-ui-MacBookAir.local>
2023-11-10 11:21:31 +0900

    api 폴더추가 + page 폴더 구조 변경

--- app/admin/(pages)/layout.tsx
+++ app/admin/(pages)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-red-300">
+    <div className="flex h-screen w-full flex-col items-center justify-center bg-white">
       {children}
     </div>
   );
